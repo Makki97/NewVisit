fa958202c4cd684aa91bf3415a1a4ba10bd43fee
Makki97 <abdulrehmaan97@outlook.com>
2020-11-09 09:04:26 +0500

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+/*
+* Theasys
+* Generated on: Sunday 28th of June 2020 04:28:07 PM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file : 'index.html',
+    path : 'http://localhost',//path to the index.html
+    theme : 'theasys',
+};
